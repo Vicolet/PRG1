b8dd4ae5ff8b22fec09f3fc245e0cd42a3b410bb
Botletellus <boletellus@prontonmail.ch>
2022-09-21 10:51:34 +0200

    labo2 terminé

--- nicolet_labo2.cpp
+++ nicolet_labo2.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
 
 int main() {
-   std::cout << "test test" << std::endl;
+   std::string prenom;
+   int age;
+   int annee_naissance;
+
+   std::cout << "Entrer votre prenom: ";
+   getline(std::cin, prenom);
+
+   std::cout << "Entrer votre age: ";
+   std::cin >> age;
+
+   annee_naissance = 2022 - age;
+
+   std::cout << "Bonjour " << prenom << "," << std::endl;
+   std::cout << "Vous avez " << age
+             << " ans et vous etes ne en " << annee_naissance << "."
+             << std::endl;
    return 0;
 }
