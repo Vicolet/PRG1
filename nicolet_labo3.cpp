1c0ec65a50c6df1d6a685d2fa8df221b9c7ef132
Botletellus <boletellus@prontonmail.ch>
2022-09-21 14:05:04 +0200

    labo 3 finito

--- nicolet_labo3.cpp
+++ nicolet_labo3.cpp
@@ -3,8 +3,43 @@
  * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
  * Date creation   : 2022-09-21
  * Laboratoire n°  : 3
- * Description     :
+ * Description     : Ce programme transforme une durée, exprimée en seconde uniquement, vers des semaines, jours,
+ *                   heures, minutes, secondes.
  * Remarque(s)     :
  * Compilateur     : gcc version 11.2.0 (MinGW-W64 x86_64-posix-seh, built by Brecht Sanders)
 */
 
+#include <iostream>
+
+const int JOUR_PAR_SEMAINE = 7;
+const int HEURE_PAR_JOUR = 24;
+const int MINUTE_PAR_HEURE = 60;
+const int SECONDE_PAR_MINUTE = 60;
+
+int main() {
+   int userValor, seconde, minute, heure, jour, semaine;
+
+   std::cout << "Introduisez un nombre de seconde: ";
+   std::cin >> userValor;
+   std::cout << std::endl;
+
+   minute = userValor / SECONDE_PAR_MINUTE;
+   heure = minute / MINUTE_PAR_HEURE;
+   jour = heure / HEURE_PAR_JOUR;
+   semaine = jour / JOUR_PAR_SEMAINE;
+
+   seconde = userValor % SECONDE_PAR_MINUTE;
+   minute = minute % MINUTE_PAR_HEURE;
+   heure = heure % HEURE_PAR_JOUR;
+   jour = jour % JOUR_PAR_SEMAINE;
+
+   std::cout << userValor << "s = "
+             << semaine << "semaine "
+             << jour << "jours "
+             << heure << "heures "
+             << minute << "minutes "
+             << seconde << "secondes "
+             << std::endl;
+
+   return 0;
+}
