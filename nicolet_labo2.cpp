eece09d83c40ac8118f15a8167fe647c8a172ccd
Botletellus <boletellus@prontonmail.ch>
2022-09-21 11:16:35 +0200

    ajout de l'en-tête

--- nicolet_labo2.cpp
+++ nicolet_labo2.cpp
@@ -1,3 +1,13 @@
+/**
+ * Nom du fichier  : nicolet_labo2.cpp
+ * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
+ * Date creation   : 2022-09-21
+ * Laboratoire n°  : 2
+ * Description     : Calcul la date de naissance à partir de l'âge
+ * Remarque(s)     :
+ * Compilateur     : gcc version 11.2.0 (MinGW-W64 x86_64-posix-seh, built by Brecht Sanders)
+*/
+
 #include <iostream>
 #include <string>
 
