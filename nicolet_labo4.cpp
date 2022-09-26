ddb138fde8cf1be41d0b3e1273a91e12b5c6abb6
Botletellus <boletellus@prontonmail.ch>
2022-09-26 13:28:30 +0200

    ajout du labo 4

---
+++ nicolet_labo4.cpp
@@ -0,0 +1,17 @@
+/**
+ * Nom du fichier  : nicolet_labo4.cpp
+ * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
+ * Date creation   : 2022-09-26
+ * Laboratoire n°  : 4
+ * Description     : Ce programme transforme une durée, exprimée en seconde uniquement, vers des semaines, jours,
+ *                   heures, minutes, secondes.
+ * Remarque(s)     :
+ * Compilateur     : gcc version 11.2.0 (MinGW-W64 x86_64-posix-seh, built by Brecht Sanders)
+*/
+
+#include <iostream>
+
+int main (){
+
+
+}
