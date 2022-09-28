e6067257798455a795a8be0462c0dc86546d89a6
Botletellus <boletellus@prontonmail.ch>
2022-09-28 13:28:45 +0200

    ajout labo6

---
+++ nicolet_labo6.cpp
@@ -0,0 +1,15 @@
+/**
+ * Nom du fichier  : nicolet_labo6.cpp
+ * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
+ * Date creation   : 2022-09-28
+ * Laboratoire n°  : 6
+ * Description     :
+ * Remarque(s)     :
+ * Compilateur     : gcc version 11.2.0 (MinGW-W64 x86_64-posix-seh, built by Brecht Sanders)
+*/
+
+#include <iostream>
+
+int main(){
+
+}
