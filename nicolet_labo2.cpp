ac655e3dc33bcbc90746102d26263b0d053baa99
Botletellus <boletellus@prontonmail.ch>
2022-09-28 14:28:47 +0200

    labo2 finito pipo

--- nicolet_labo2.cpp
+++ nicolet_labo2.cpp
@@ -3,7 +3,7 @@
  * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
  * Date creation   : 2022-09-21
  * Laboratoire n°  : 2
- * Description     : Calcul la date de naissance à partir de l'âge
+ * Description     : Calcul la date de naissance à partir de l'âge.
  * Remarque(s)     :
  * Compilateur     : gcc version 11.2.0 (MinGW-W64 x86_64-posix-seh, built by Brecht Sanders)
 */
@@ -17,7 +17,7 @@ int main() {
 
    std::string prenom;
    int age;
-   int annee_naissance;
+   int anneeNaissance;
 
    std::cout << "Entrer votre prenom: ";
    getline(std::cin, prenom);
@@ -25,10 +25,10 @@ int main() {
    std::cout << "Entrer votre age: ";
    std::cin >> age;
 
-   annee_naissance = CETTE_ANNEE - age;
+   anneeNaissance = CETTE_ANNEE - age;
 
    std::cout << "Bonjour " << prenom << "," << std::endl;
-   std::cout << "Vous avez " << age << " ans et vous etes ne en " << annee_naissance << "." << std::endl;
+   std::cout << "Vous avez " << age << " ans et vous etes ne en " << anneeNaissance << "." << std::endl;
 
    return 0;
 
