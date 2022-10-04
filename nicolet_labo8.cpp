763f95062d2d5d3133893d63ef2f01b0be7790a8
Botletellus <boletellus@prontonmail.ch>
2022-10-04 11:36:01 +0200

    labo8 modif

--- nicolet_labo8.cpp
+++ nicolet_labo8.cpp
@@ -3,32 +3,38 @@
  * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
  * Date creation   : 2022-10-04
  * Laboratoire n°  : 8
- * Description     : Ce programme permet de transformer des degrés Celsius en Fahrenheit et vice versa.
+ * Description     : Ce programme permet de trouver un quadrilatère en fonction des coordonnées entrées par
+ *                   l'utilisateur et indique si le quadrilatère est un carré, un rectangle, un losange,
+ *                   un parallélogramme, un trapèze ou aucune de ces formes.
  * Remarque(s)     :
  * Compilateur     : gcc version 11.2.0 (MinGW-W64 x86_64-posix-seh, built by Brecht Sanders)
 */
 
-/*Écrire un programme qui lit les coordonnées x et y des quatre sommets d’un quadrilatère (supposé
-donné dans l’ordre de ses sommets, avec des coordonnées entières) et qui indique si le quadrilatère
-est un carré, un rectangle, un losange, un parallélogramme, un trapèze ou aucune de ces formes.
-Exemple de réponse du programme selon les coordonnées fournies par l’utilisateur :
-(0, 0)(0, 1)(1, 1)(1, 0) : Le quadrilatère est un carré
-(0, 1)(2, 2)(4, 1)(2, 0) : Le quadrilatère est un losange
-(1, 0)(3, 0)(3, 1)(1, 1) : Le quadrilatère est un rectangle
-(0, 0)(1, 0)(2, 1)(1, 1) : Le quadrilatère est un parallélogramme
-(1, 0)(5, 0)(4, 1)(3, 1) : Le quadrilatère est un trapèze
-   On rappelle que 2 vecteurs (ax , ay )(bx , by ) sont parallèles si axby = aybx et qu’ils sont
-perpendiculaires si axbx + ayby = 0 */
+
 
 #include <iostream>
 
 int main() {
 
-   //un carré, un rectangle, un losange, un parallélogramme, un trapèze ou aucune de ces formes
+   int x1, y1, x2, y2, x3, y3, x4, y4;
 
-   //2 vecteurs (ax , ay )(bx , by )
-   //parallèles si axby = aybx
+   std::cout << "Entrer 4 coordonnees sous forme (x,y) exemple -> 9 1" << std::endl;
 
-   //perpendiculaires si axbx + ayby = 0
+   std::cout << "premiere Coordonnees : ";
+   std::cin >> x1 >> y1;
+   std::cout << std::endl;
+
+   std::cout << "deuxieme Coordonnees : ";
+   std::cin >> x2 >> y2;
+   std::cout << std::endl;
+
+   std::cout << "troisieme Coordonnees : ";
+   std::cin >> x3 >> y3;
+   std::cout << std::endl;
+
+   std::cout << "quatrieme Coordonnees : ";
+   std::cin >> x4 >> y4;
+   std::cout << std::endl;
+
 }
 
