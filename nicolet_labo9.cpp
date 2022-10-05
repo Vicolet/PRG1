f369962cf65378db6999a35d506c356b64c61812
Botletellus <boletellus@protonmail.ch>
2022-10-05 20:13:10 +0200

    ajout labo9

---
+++ nicolet_labo9.cpp
@@ -0,0 +1,17 @@
+/**
+ * Nom du fichier  : nicolet_labo9.cpp
+ * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
+ * Date creation   : 2022-10-04
+ * Laboratoire n°  : 9
+ * Description     :
+ * Remarque(s)     :
+ * Compilateur     : gcc version 11.2.0 (MinGW-W64 x86_64-posix-seh, built by Brecht Sanders)
+*/
+
+#include <iostream>
+
+int main() {
+
+
+}
+
