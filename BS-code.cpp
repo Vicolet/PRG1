a5451ee7130b6a7883625b95f895416619b903a1
Botletellus <boletellus@protonmail.ch>
2022-10-12 23:44:23 +0200

    pushBS

--- BS-code.cpp
+++ BS-code.cpp
@@ -1,54 +1,37 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 
-using namespace std;
+const double PRECISION = 1.0E-10;
 
 int main() {
-   char c = 'a';
-   cout << "(" << c++ << ")\n";
-   for (int i = 1; i <= 3; ++i) {
-      for (int j = 1; j <= 4; ++j) {
-         cout << "*";
+   int iterationHeron;
+   double valeurUtilisateur = 0.0, approximation;
+   double division;
+   double precision;
+
+   std::cout << std::setprecision(10);
+   std::ofstream labo10;
+   labo10.open("nicolet_labo10_csv.csv");
+   labo10 << "valeur de x, iteration \n";
+
+   for (double i = 1; i < 1E9; i *= 10) {
+      iterationHeron = 0;
+      valeurUtilisateur = i;
+      approximation = 0.0;
+      division = 2.0;
+      precision = 1.0;
+
+      while (precision > PRECISION || precision < -PRECISION) {
+         iterationHeron++;
+         approximation = valeurUtilisateur / division;
+         division = (approximation + division) / 2;
+         precision = approximation - division;
       }
-      cout << endl;
+      labo10 << valeurUtilisateur << "," << iterationHeron << "\n";
    }
 
-   cout << "\n(" << c++ << ")\n";
-   for (int i = 0; i < 4; ++i) {
-      for (int j = 0; j < 3; ++j) {
-         cout << "*";
-      }
-      cout << endl;
-   }
+   labo10.close();
+   return 0;
 
-   cout << "\n(" << c++ << ")\n";
-   for (int i = 1; i <= 4; ++i) {
-      for (int j = 1; j <= i; ++j) {
-         cout << "*";
-      }
-      cout << endl;
-   }
-
-   cout << "\n(" << c++ << ")\n";
-   for (int i = 1; i <= 3; ++i) {
-      for (int j = 1; j <= 5; ++j) {
-         if (j % 2 == 0) {
-            cout << "*";
-         } else {
-            cout << "_";
-         }
-      }
-      cout << endl;
-   }
-
-   cout << "\n(" << c++ << ")\n";
-   for (int i = 1; i <= 3; ++i) {
-      for (int j = 1; j <= 5; ++j) {
-         if ((i + j) % 2 == 0) {
-            cout << "*";
-         } else {
-            cout << " ";
-         }
-      }
-      cout << endl;
-   }
 }
