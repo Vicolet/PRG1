7dea711ac1e6865dfef2c912588c47240256ae38
Vicolet <victor.nicolet@heig-vd.ch>
2022-10-15 15:18:36 +0200

    pushBS

--- BS-code.cpp
+++ BS-code.cpp
@@ -12,37 +12,19 @@
 #include <iostream>
 #include <iomanip>
 
-/**
- * Cette constante nous permet donner une précision de 10 chiffre après la virgule.
- */
 const double PRECISION = 1.0E-10;
+const double ESTIMATION_DE_DEPART = 2.0;
 
 int main() {
 
-   /**
-    * Nous permet de donner une précision à l'affichage (et d'afficher 10 chiffres
-    */
-   std::cout << std::setprecision(10);
+   std::cout << std::setprecision(11);
 
    bool flagNegatif = false;
    int iterationHeron = 0;
-   double valeurUtilisateur, approximation = 0.0;
-
-   /**
-    * Variable "division" initialisé à 2, car 2 est demandé comme première approximation.
-    */
+   double valeurUtilisateur = 0.0, approximation =0.0;
    double division = 2.0;
-
-   /**
-    * Variable "precision" initialisé à 1, car elle doit rentrer dans la boucle pour avoir ça vraie valeur.
-    * (peut être initialisé avec toutes les valeurs sauf 0)
-    */
    double precision = 1.0;
 
-   /**
-    * Cette boucle while permet de tester si la valeur entrée par l'utilisateur est négative, si la valeur est negative
-    * le programme demande une nouvelle valeur à l'utilisateur.
-    */
    while (not flagNegatif) {
       std::cout << "Entrer un nombre dont vous voulez connaitre la racine carree : ";
       std::cin >> valeurUtilisateur;
@@ -50,16 +32,16 @@ int main() {
 
       if (valeurUtilisateur < 0) {
          std::cout << "Le nombre ne doit pas etre negatif ! " << std::endl;
+      } else if (valeurUtilisateur == 0) {
+         std::cout << "La racine de " << valeurUtilisateur << " est : " << approximation << std::endl;
+         std::cout << "Il y a " << iterationHeron << " iterations." << std::endl;
+         return 0;
       } else {
          flagNegatif = true;
       }
    }
 
-   /**
-    * Cette boucle while permet d'effectuer l'algorithme d'Héron d’Alexandrie en fonction de la précision voulue, la
-    * précision voulue est donné avant le main avec la constante PRECISION
-    */
-   while (precision > PRECISION || precision < -PRECISION) {
+   while (precision > PRECISION * approximation|| precision < -PRECISION * approximation) {
       iterationHeron++;
       approximation = valeurUtilisateur / division;
       division = (approximation + division) / 2.0;
