1c7692aa7ce231a2453b7fc19451a8878af6885b
Vicolet <victor.nicolet@heig-vd.ch>
2022-10-18 11:37:31 +0200

    pushBS

--- exercice-recueil-taillard/exercice-34-plus-grand-diviseur-commun.cpp
+++ exercice-recueil-taillard/exercice-34-plus-grand-diviseur-commun.cpp
@@ -1,4 +1,16 @@
-//
-// Created by lord on 17.10.2022.
-//
+#include <iostream>
 
+int additionFraction(int dividende1, int diviseur1, int dividende2, int diviseur2);
+
+int main() {
+   int num1 = 23412;
+   int num2 = 392;
+
+   std::cout << pgdc(num1, num2) << std::endl;
+
+   return 0;
+}
+
+int additionFraction(int dividende1, int diviseur1, int dividende2, int diviseur2){
+
+}
