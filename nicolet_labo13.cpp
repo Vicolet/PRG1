8aea503a1651a778ab73346d5bd43d92a395f4cd
Vicolet <victor.nicolet@heig-vd.ch>
2022-10-18 11:45:25 +0200

    ajout du labo12 et labo13

--- nicolet_labo13.cpp
+++ nicolet_labo13.cpp
@@ -1,4 +1,22 @@
-//
-// Created by lord on 18.10.2022.
-//
+/**
+ * Nom du fichier  : nicolet_labo13.cpp
+ * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
+ * Date creation   : 2022-10-18
+ * Laboratoire n°  : 13
+ * Description     : Ce programme imprime des carrés imbriqués avec les lettres de l’alphabet.
+ * Remarque(s)     :
+ * Compilateur     : gcc version 11.2.0 (MinGW-W64 x86_64-posix-seh, built by Brecht Sanders)
+*/
+
+#include <iostream>
+
+int main(){
+   int valeurUtilisateur = 0;
+
+   std::cout << "Introduiser le nombre de carre voulu : " << std::endl;
+   std::cin >> valeurUtilisateur;
+
+
+
+}
 
