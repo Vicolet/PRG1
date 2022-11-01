e0595759ccba64b16ca00353ce2c806b922b5791
Vicolet <victor.nicolet@heig-vd.ch>
2022-11-01 10:53:33 +0100

    ajout labo14

---
+++ nicolet_labo14.cpp
@@ -0,0 +1,15 @@
+/**
+ * Nom du fichier  : nicolet_labo14.cpp
+ * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
+ * Date creation   : 2022-11-01
+ * Laboratoire n°  : 14
+ * Description     : Ce programme permet d'implémenter des fonctions simples.
+ * Remarque(s)     :
+ * Compilateur     : gcc version 11.2.0 (MinGW-W64 x86_64-posix-seh, built by Brecht Sanders)
+*/
+
+#include <iostream>
+
+int main(){
+   return 0;
+}
