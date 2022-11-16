699a3e9e021bc442019b4c01a8c91ca0de344e1c
Vicolet <victor.nicolet@heig-vd.ch>
2022-11-16 12:36:41 +0100

    ajout nicolet_labo16

---
+++ cmake-build-debug/CMakeFiles/PRG1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/41788/OneDrive/HEIG-VD/2022-2023/PRG1/BS-code.cpp" "CMakeFiles/PRG1.dir/BS-code.cpp.obj" "gcc" "CMakeFiles/PRG1.dir/BS-code.cpp.obj.d"
+  "C:/Users/41788/OneDrive/HEIG-VD/2022-2023/PRG1/exercice-recueil-taillard/exercice-59-tous-pairs.cpp" "CMakeFiles/PRG1.dir/exercice-recueil-taillard/exercice-59-tous-pairs.cpp.obj" "gcc" "CMakeFiles/PRG1.dir/exercice-recueil-taillard/exercice-59-tous-pairs.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PRG1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PRG1.dir/BS-code.cpp.obj"
+  "CMakeFiles/PRG1.dir/BS-code.cpp.obj.d"
+  "CMakeFiles/PRG1.dir/exercice-recueil-taillard/exercice-59-tous-pairs.cpp.obj"
+  "CMakeFiles/PRG1.dir/exercice-recueil-taillard/exercice-59-tous-pairs.cpp.obj.d"
+  "PRG1.exe"
+  "PRG1.exe.manifest"
+  "PRG1.pdb"
+  "libPRG1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PRG1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
