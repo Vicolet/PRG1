88e461075d5392317294b9887677e64eb03f6fe5
Vicolet <victor.nicolet@heig-vd.ch>
2022-11-22 23:15:37 +0100

    labo17 finito pipo

--- nicolet_labo17/nicolet_labo17.cpp
+++ nicolet_labo17/nicolet_labo17.cpp
@@ -21,35 +21,45 @@
  */
 std::string changementBase(size_t val, size_t base = 10, size_t typo = 0);
 
-std::vector<std::string> defaut = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"};
-std::vector<std::string> nombre = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"};
-std::vector<std::string> nombre = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"};
-
+std::vector<std::string> nombre0 = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14",
+                                    "15", "16", "17", "18", "19"};
+std::vector<std::string> nombre1 = {"⓪", "①", "②", "③", "④", "⑤", "⑥", "⑦", "⑧", "⑨", "⑩", "⑪", "⑫", "⑬", "⑭",
+                                    "⑮", "⑯", "⑰", "⑱", "⑲"};
+std::vector<std::string> nombre2 = {"⓿", "❶", "❷", "❸", "❹", "❺", "❻", "❼", "❽", "❾", "❿", "⓫", "⓬", "⓭", "⓮",
+                                    "⓯", "⓰", "⓱", "⓲", "⓳"};
+std::vector<std::string> nombre3 = {"zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf",
+                                    "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept",
+                                    "dix-huit", "dix-neuf"};
+std::vector<std::vector<std::string>> typoChoix = {nombre0, nombre1, nombre2, nombre3};
 
 int main() {
-   size_t valeurUtilisateur, baseUtilisateur, typoUtilisateur;
-   std::cout << "Donner un nombre et la base dans la quelle vous voulez le transformer\n"
-             << "sous le format 1234(valeur) 2a20(base) 0a2(typo): ";
-   std::cin >> valeurUtilisateur >> baseUtilisateur >> typoUtilisateur;
 
-   if (valeurUtilisateur < 2 or valeurUtilisateur > 20) {
-      std::cout << "Erreur: base invalide";
-      return 0;
-   } else if (typoUtilisateur < 0 or typoUtilisateur > 3) {
-      std::cout << "Table de correspondance non disponible";
-      return 0;
-   }
+    size_t valeurUtilisateur, baseUtilisateur, typoUtilisateur;
 
-   std::string test = changementBase(valeurUtilisateur, baseUtilisateur, typoUtilisateur);
+    std::cout << "Donner un nombre et la base dans la quelle vous voulez le transformer\n"
+              << "sous le format 1234(valeur) 2a20(base) 0a2(typo): ";
+    std::cin >> valeurUtilisateur >> baseUtilisateur >> typoUtilisateur;
 
-   return 0;
+    if (baseUtilisateur < 2 or baseUtilisateur > 20) {
+        std::cout << "Erreur: base invalide";
+        return 0;
+    } else if (typoUtilisateur < 0 or typoUtilisateur > 3) {
+        std::cout << "Table de correspondance non disponible";
+        return 0;
+    }
+
+    std::string test = changementBase(valeurUtilisateur, baseUtilisateur, typoUtilisateur);
+    std::cout << test << std::endl;
+
+    return 0;
 }
 
 
 std::string changementBase(size_t val, size_t base, size_t typo) {
-   std::string str;
-   while (val / base) {
-
-   }
-   return str;
+    std::string str;
+    while (val != 0) {
+        str = typoChoix[typo][val % base] + str;
+        val = val / base;
+    }
+    return str;
 }
