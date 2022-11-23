2b58cb64c808a3f3305389888c0c93f9709fa12a
Vicolet <victor.nicolet@heig-vd.ch>
2022-11-23 14:39:37 +0100

    avancé labo18

--- nicolet_labo18/nicolet_labo18.cpp
+++ nicolet_labo18/nicolet_labo18.cpp
@@ -10,26 +10,31 @@
 
 #include "nicolet_labo18.h"
 
-std::string readFile (const std::string& fileName);
+std::string readFile(const std::string &fileName);
 
 int main() {
    std::string nomFichier = "la_comedie_humaine.txt";
    std::string fichier = readFile(nomFichier);
+   size_t index = 0;
+   int compteur = 0;
+   while ((index = fichier.find(' ', index + 1)) != std::string::npos) {
+      compteur++;
+   }
+   std::cout << compteur << std::endl;
    std::vector<Mot> vectorMot;
 
 
 }
 
-std::string readFile (const std::string& fileName){
-   std::string file;
+std::string readFile(const std::string &fileName) {
    std::ifstream input_file(fileName, std::ios::in);
    if (!input_file) {
-      std::cerr << file << " not found!\n";
+      std::cerr << fileName << " not found!\n";
       exit(-1);
    }
-   std::string original;
-   std::getline(input_file, original, '\0');
+   std::string file;
+   std::getline(input_file, file, '\0');
    input_file.close();
-   return original;
+   return file;
 }
 
