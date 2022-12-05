5c586f598d4aa9a6f19fc849c8174f1d217bcad5
Vicolet <victor.nicolet@heig-vd.ch>
2022-12-05 14:50:46 +0100

    pushBS

--- exercice-recueil-taillard/exercice-73-nombres-rationnels-base/Rationnel.h
+++ exercice-recueil-taillard/exercice-73-nombres-rationnels-base/Rationnel.h
@@ -4,7 +4,30 @@
 
 #ifndef EXERCICE_73_NOMBRES_RATIONNELS_BASE_RATIONNEL_H
 #define EXERCICE_73_NOMBRES_RATIONNELS_BASE_RATIONNEL_H
-class Rationnel{
+
+class Rationnel {
+public:
+   Rationnel(int denom, int numer) {
+      this->denominateur = denom;
+      this->numerateur = numer;
+   }
+
+   Rationnel &operator+();
+
+   Rationnel &operator-();
+
+   Rationnel operator*=();
+
+   Rationnel operator*();
+
+   Rationnel operator/=();
+
+   Rationnel operator/();
+
+private:
+   int denominateur = 0;
+   int numerateur = 0;
 
 };
+
 #endif //EXERCICE_73_NOMBRES_RATIONNELS_BASE_RATIONNEL_H
