dce383c6fd4e5a0b10469752cedd4a46991a6c4d
Vicolet <victor.nicolet@heig-vd.ch>
2022-12-21 18:08:12 +0100

    avancé labo21

--- nicolet_labo21/Prime.cpp
+++ nicolet_labo21/Prime.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "Prime.hpp"
+
+Uint exponentiationModulaire(Uint base, Uint exposant, Uint modulo) {
+   Uint r(1);
+
+   while (exposant > 0) {
+      if (exposant % 2 == 0) {
+         base = base * base % modulo;
+         exposant = exposant / 2;
+      } else {
+         r = r * base % modulo;
+         exposant--;
+      }
+   }
+   return r;
+}
+
+bool prime(Uint nombrePremier) {
+   if (nombrePremier < 2) {
+      return false;
+   }
+   if (nombrePremier == 2) {
+      return true;
+   }
+
+   for (uint64_t i = 0; i != 10; i++) {
+      auto gen_int1_3 = std::bind(std::uniform_int_distribution<uint64_t>(1, uint64_t (nombrePremier - 1)), std::mt19937(987));
+      uint64_t nombreAleatoire = gen_int1_3();
+      Uint alea(nombreAleatoire);
+
+      if (exponentiationModulaire(alea, nombrePremier - 1, nombrePremier) != 1) {
+         return false;
+      }
+
+      Uint q = 1, u = nombrePremier - 1;
+
+      while ((u % 2 == 0) && (q == 1)) {
+         u = u / 2;
+         q = exponentiationModulaire(alea, u, nombrePremier);
+
+         if ((q != 1) && (q != nombrePremier - 1)) {
+            return false;
+         }
+      }
+   }
+   return true;
+}
--- nicolet_labo21/Prime.hpp
+++ nicolet_labo21/Prime.hpp
@@ -1,5 +1,5 @@
 /**
- * Nom du fichier  : prime.hpp
+ * Nom du fichier  : Prime.hpp
  * Auteur(s)       : Victor Nicolet <victor.nicolet@heig-vd.ch>
  * Date creation   : 2022-12-12
  * Laboratoire n°  : 21
@@ -9,5 +9,22 @@
 */
 
 #include "Uint.hpp"
+#include <iostream>
+#include <functional>
+#include <random>
 
+/**
+ *
+ * @param base
+ * @param exposant
+ * @param modulo
+ * @return
+ */
+Uint exponentiationModulaire(Uint base, Uint exposant, Uint modulo);
 
+/**
+ *
+ * @param nombrePremier ce paramètre représente la valeur sur la quelle on veut faire le test de primalité.
+ * @return un booléen pour savoir si le nombre tester est premier True si premier False si pas premier.
+ */
+bool prime(Uint nombrePremier);
